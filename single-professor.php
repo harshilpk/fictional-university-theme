a33fe8bddcfd15c8dfd22d4a720653796d0ff496
Harshil <harshilpk@gmail.com>
2018-09-07 15:04:46 -0700

    updated page banner for a professor

--- single-professor.php
+++ single-professor.php
@@ -5,7 +5,7 @@
     while(have_posts()) {
         the_post(); ?>
         <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_baackground_image'); echo $pageBannerImage['size']['pageBanner']; ?>)"></div>
+    <div class="page-banner__bg-image" style="background-image: url(<?php $pageBannerImage = get_field('page_banner_background_image'); echo $pageBannerImage['sizes']['pageBanner']; ?>)"></div>
     <div class="page-banner__content container container--narrow">
       <h1 class="page-banner__title"><?php the_title(); ?></h1>
       <div class="page-banner__intro">
